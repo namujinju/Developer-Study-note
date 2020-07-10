58f17f19bea614633a1f3daa84fb63dc8bb1c9fe
namujinju <59328810+namujinju@users.noreply.github.com>
2020-07-10 23:50:05 +0900

    Update build_palindrome.py

--- Algorithm Study/codewars/build_palindrome.py
+++ Algorithm Study/codewars/build_palindrome.py
@@ -3,7 +3,7 @@ def build_palindrome(s):
         if list(s[i:]) == list(reversed(s[i:])): # s의 오른쪽 끝부터 왼쪽 i번째까지 회문인지 검증
             s += "".join(reversed(s[:i]))
             return s
-        elif list(s[:len(s)-i]) == list(reversed(s[:len(s)-i])): # s의 왼쪽 끝부터 len(s) - i번째까지 회문인지 검증
+        elif list(s[:len(s)-i]) == list(reversed(s[:len(s)-i])): # s의 왼쪽 끝부터 len(s) - i-1번째까지 회문인지 검증
             s = "".join(reversed(s[len(s)-i:])) + s
             return s
         else:
