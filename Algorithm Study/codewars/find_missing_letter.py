61c489510c58e27a6cddebc389f047abbaff5fd6
namujinju <59328810+namujinju@users.noreply.github.com>
2020-07-22 14:58:34 +0900

    Add files via upload

--- Algorithm Study/codewars/find_missing_letter.py
+++ Algorithm Study/codewars/find_missing_letter.py
@@ -1,5 +1,11 @@
 def find_missing_letter(chars):
-    return chr(ord(chars[-1]) * (ord(chars[-1])+1) // 2 - (ord(chars[0])-1) * ord(chars[0]) // 2 - sum(list(map(lambda x: ord(x), chars))))
+    i=0
+    while ord(chars[i]) == ord(chars[i+1]) - 1:
+        i += 1
+    return chr(ord(chars[i])+1)
+
+
+    
 #  
 chars = ["a","b","c","d","f"]
 #  
