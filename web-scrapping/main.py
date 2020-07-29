37d1e9a7778f9534bba1f557058d2d50d5cf90de
namujinju <59328810+namujinju@users.noreply.github.com>
2020-07-29 15:26:43 +0900

    web-scrapping 폴더 이동

--- web-scrapping/main.py
+++
@@ -1,7 +0,0 @@
-from indeed import extract_indeed_pages, extract_indeed_jobs
-
-last_indeed_pages = extract_indeed_pages()
-# last_indeed_pages = 20
-
-indeed_jobs = extract_indeed_jobs(last_indeed_pages)
-print(indeed_jobs)
