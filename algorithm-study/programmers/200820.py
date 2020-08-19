e99be541f5b82fb4420f7d24b782145d7ea05f1e
namujinju <59328810+namujinju@users.noreply.github.com>
2020-08-20 02:50:22 +0900

    Update 200820.py

--- algorithm-study/programmers/200820.py
+++ algorithm-study/programmers/200820.py
@@ -7,6 +7,19 @@
 #     return len([y-x for (x, y) in comb(arr, 2) if y-x == n])
 # 시간 초과
 
+# 1
+# 1 + 2
+# 1 + 2 + 3
+# 1 + 2 + 3 + 4 < 15
+
+# 1 + 2 + 3 + 4 + 5 == 15 -> answer += 1
+
+# 1 + 2 + 3 + 4 + 5 + 6 > 15 -> 1 제거
+# 2 + 3 + 4 + 5 + 6 > 15 -> 2 제거
+# 3 + 4 + 5 + 6 > 15 -> 3 제거
+
+# 4 + 5 + 6 == 15 -> answer += 1
+
 
 def solution(n):
     answer = 0
@@ -16,15 +29,13 @@ def solution(n):
     while i <= n+1:
         if sums > n:
             sums -= arr.pop(0)
+            continue
         elif sums == n:
             answer += 1
-            arr.append(i)
-            sums += i
-            i += 1
-        else:
-            arr.append(i)
-            sums += i
-            i += 1
+
+        arr.append(i)
+        sums += i
+        i += 1
 
     return answer
 
