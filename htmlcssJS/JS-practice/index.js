790b21e5318a326e434dc836f5f678a608037a8c
namujinju <59328810+namujinju@users.noreply.github.com>
2020-12-26 19:47:11 +0900

    스터디 설명

--- htmlcssJS/JS-practice/index.js
+++ htmlcssJS/JS-practice/index.js
@@ -12,10 +12,10 @@ console.log(arr3); // [3, 4]
 //splice는 원본 변경
 
 //push, pop
-const apple = ["a", "p", "p", "l", "e"];
+const apple = ["a", "p", "p", "l", "e"]; // <-- push
 apple.push("d");
 console.log(apple); // ["a", "p", "p", "l", "e", "d"]
-apple.pop();
+apple.pop(); // 오른쪽에서 없앤다
 apple.pop();
 apple.pop();
 console.log(apple); // ["a", "p", "p"]
@@ -25,12 +25,12 @@ console.log(apple); // ["a", "p", "p"]
 // 다음 배열의 각 원소를 제곱한 배열을 만들어라.(map)
 let my_arr = [3, 6, 8, 0, 13, 22, 5];
 let my_arr2 = my_arr.map((x) => x ** 2);
-console.log(my_arr2);
+console.log(my_arr2); // [9,36,64, ....]
 
 // 다음 배열의 각 원소 중 3의 배수를 제거하라.(filter)
 let arr = [2, 5, 6, 8, 1, 9, 15, 7];
 let arr_ = arr.filter((x) => x % 3 != 0);
-console.log(arr_);
+console.log(arr_); // [2,5,8,1,7]
 
 //template literal
 
